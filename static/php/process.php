418a46fae1fc1a7b7e707bc32ae6192f0a5502a4
alanbares <neloice@gmail.com>
2015-12-31 00:26:47 -0800

    Add header to specify non functional contact form and php code for form validation.

--- static/php/process.php
+++ static/php/process.php
@@ -11,4 +11,25 @@ $from = 'Contact form'
 $to = 'alanbares@gmail.com'
 $subject = 'Message from contact';
 
-$body = "From: $name\n E-Mail: $email\n Message:\n $message";
+$body = 'From: $name\n E-Mail: $email\n Message:\n $message';
+
+// Form Validation
+
+# Name field
+if (!$_POST['name']) {
+    $errName = 'Please enter your name';
+}
+
+# Email field
+if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    $errEmail = 'Please enter a valid email address';
+}
+
+# Spam Bot
+if ($human !== 5) {
+    $errHuman = 'Can\'t tell if human'
+}
+
+
+
+?>
