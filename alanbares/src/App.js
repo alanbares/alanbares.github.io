15b26c32062f16577446f51568020e13572f6273
alanbares <neloice@gmail.com>
2016-09-26 14:26:16 -0700

    Add navigation component

---
+++ alanbares/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navigation from './components/navbar'
+
+class App extends Component {
+  render() {
+    return (
+      <Navigation />
+    )
+
+  }
+}
+
+export default App;
---
+++ alanbares/src/components/navbar.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+
+function Navigation() {
+  return (
+      <div id="background">
+        <nav className="navbar navbar-default navbar-fixed-top">
+          <div className="col-md-12">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                </button>
+                <a className="navbar-brand highlight" href="#">Alan Bares</a>
+            </div>
+            <div className="collapse navbar-collapse navHeaderCollapse">
+              <ul className="nav navbar-nav navbar-right highlight">
+                <li><a href="#top">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#contact">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+  );
+}
+
+
+
+
+
+export default Navigation;
